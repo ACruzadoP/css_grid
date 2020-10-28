2c340cf45af3b87f337da168ac2a5f3151975821
Alberto <albertocruzadopleite@gmail.com>
2020-10-28 14:18:19 +0100

    The exercise was completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <main className="main1">
+        <div className="box1">
+          <h1>Box 1</h1>
+        </div>
+        <div className="box2">
+          <h1>Box 2</h1>
+        </div>
+        <div className="box3">
+          <h1>Box 3</h1>
+        </div>
+      </main>
+      <br></br>
+      <main className="main2">
+        <div className="box21">
+          <h1>Box 1</h1>
+        </div>
+        <div className="box22">
+          <h1>Box 2</h1>
+        </div>
+        <div className="box23">
+          <h1>Box 3</h1>
+        </div>
+      </main>
+      <br></br>
+      <main className="responsive">
+        <div className="box31">
+          <h1>Box 1</h1>
+        </div>
+        <div className="box32">
+          <h1>Box 2</h1>
+        </div>
+        <div className="box33">
+          <h1>Box 3</h1>
+        </div>
+        <div className="box34">
+          <h1>Box 4</h1>
+        </div>
+        <div className="box35">
+          <h1>Box 5</h1>
+        </div>
+      </main>
+    </React.Fragment>
   );
 }
 
